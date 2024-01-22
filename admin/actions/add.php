0746921ce1a68d1495f8fbea418992da1e13f7eb
Asmae <154051652+asmae-hanine@users.noreply.github.com>
2024-01-22 14:02:07 +0100

    mergin

--- admin/actions/add.php
+++ admin/actions/add.php
@@ -96,6 +96,7 @@ include('../imports/dashboard-header.php');
         <div id="layoutSidenav_content">
 
             <!-- the form  -->
+
             <main>
                 <div class="container-fluid px-4">
                     <h1 class="mt-4">Add New Product</h1>
